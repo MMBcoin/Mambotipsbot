9b46811e13ac574f177d6325333cd139903d1686
Mambo Coin <40155210+MMBcoin@users.noreply.github.com>
2018-07-16 08:14:22 -0500

    update cmc.py

--- cmc.py
+++ cmc.py
@@ -3,8 +3,8 @@ import json
 from utils import utils
 from decimal import *
 
-grlcpriceurl = 'https://api.coinmarketcap.com/v1/ticker/garlicoin/'
-dashpriceurl = 'https://api.coinmarketcap.com/v1/ticker/dash/'
+grlcpriceurl = 'https://graviex.net//api/v2/tickers/mmbbtc.json'
+dashpriceurl = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
 
 utils = utils()
 cursor = utils.get_mysql_cursor()
@@ -14,17 +14,17 @@ cursor.execute(sql)
 
 response = urllib.urlopen(grlcpriceurl)
 data = json.loads(response.read())
-grlcprice = round(Decimal(data[0]['price_usd']),8)
+grlcprice = round(Decimal(data[0]['last']),8)
 
 response = urllib.urlopen(dashpriceurl)
 data = json.loads(response.read())
 dashprice = round(Decimal(data[0]['price_usd']),8)
 
 sql = "INSERT INTO rates (pair,rate) VALUES (%s, %s)"
-pair = "GRLC/DASH"
+pair = "MMB/BTC"
 rate = grlcprice/dashprice
 cursor.execute(sql, (pair,rate,))
 
-pair = "DASH/GRLC"
+pair = "BTC/MMB"
 rate = dashprice/grlcprice
 cursor.execute(sql, (pair,rate,))
